78a0269a270e0c8e2a6c1cde3c353b61a7327581
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-05-01 13:08:31 -0700

    Updates

---
+++ precache-manifest.3905e7fc038ed7cbfe9c3255b43e527b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd62971a31127a72f4261093735f0bbb",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "811b1ec0f4fc01971431",
+    "url": "/Memory-Game/static/css/main.3f0efedf.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Memory-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "811b1ec0f4fc01971431",
+    "url": "/Memory-Game/static/js/main.d82cae0c.chunk.js"
+  },
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "3cb23685e125b1939e84092b704f4085",
+    "url": "/Memory-Game/static/media/bob-bob-s-burgers-12489-1680x1050.3cb23685.jpg"
+  }
+]);
